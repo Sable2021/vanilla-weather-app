d8613f1f744a7c8fd2d171d15cbead9a87daacaf
Sable2021 <l_sable@ukr.net>
2023-10-17 17:19:27 +0300

    Showing the currrent temperature

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 function displayTemperature(response) {
     console.log(response.data.main.temp);
     let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    document.querySelector("#city").innerHTML = response.data.name;
-
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
